cddd0ad3ae4b9e2511dc91200622dd8f455da610
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-10-28 21:28:24 -0300

    feat: updated message screens at login and changed email link

--- src/controller/TelaLoginController.java
+++ src/controller/TelaLoginController.java
@@ -22,6 +22,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 /**
@@ -65,6 +67,12 @@ public class TelaLoginController implements Initializable {
                 alert.setTitle("Mensagem do Programa");
                 alert.setHeaderText("Conectado ao sistema com sucesso!");
                 alert.setContentText("Seja bem-vindo(a) " +clientConnection.resultset.getString("nome_usuario"));
+                alert.getDialogPane().getStylesheets().add(getClass().getResource("/style/alert.css").toExternalForm());
+                alert.getDialogPane().getStyleClass().add("custom-alert");
+                ImageView icon = new ImageView(new Image(String.valueOf(this.getClass().getResource("/icons/Information.png"))));
+                icon.setFitHeight(48);
+                icon.setFitWidth(48);
+                alert.getDialogPane().setGraphic(icon);
                 alert.showAndWait();
 
                 Stage newStage = new Stage();
@@ -82,6 +90,12 @@ public class TelaLoginController implements Initializable {
                 alert.setTitle("Mensagem do Programa");
                 alert.setHeaderText("Usuário não encontrado!");
                 alert.setContentText("Verifique se o nome de usuário ou senha está correto.");
+                alert.getDialogPane().getStylesheets().add(getClass().getResource("/style/alert.css").toExternalForm());
+                alert.getDialogPane().getStyleClass().add("custom-alert");
+                ImageView icon = new ImageView(new Image(String.valueOf(this.getClass().getResource("/icons/Warning.png"))));
+                icon.setFitHeight(48);
+                icon.setFitWidth(48);
+                alert.getDialogPane().setGraphic(icon);
                 alert.showAndWait();
             }
         } catch (SQLException error) {
@@ -89,13 +103,19 @@ public class TelaLoginController implements Initializable {
             alert.setTitle("Mensagem do Programa");
             alert.setHeaderText("Dados digitados não encontrados!");
             alert.setContentText(error.toString());
+            alert.getDialogPane().getStylesheets().add(getClass().getResource("/style/alert.css").toExternalForm());
+            alert.getDialogPane().getStyleClass().add("custom-alert");
+            ImageView icon = new ImageView(new Image(String.valueOf(this.getClass().getResource("/icons/Warning.png"))));
+            icon.setFitHeight(48);
+            icon.setFitWidth(48);
+            alert.getDialogPane().setGraphic(icon);
             alert.showAndWait();
         }
     }
     
     @FXML
     void openSuportLink() throws URISyntaxException, IOException {
-        Desktop.getDesktop().browse(new URI("https://www.gmail.com"));
+        Desktop.getDesktop().browse(new URI("malito:mjorgesantana2007@gmail.com"));
     }
     
     public void onMouseDragOverBtnLogin () {
