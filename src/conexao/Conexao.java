8a5fb6cac3dd913adfc0820d64ddd9a6660e4e2d
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-11-06 20:13:49 -0300

    feat: Conexao class has been improved

--- src/conexao/Conexao.java
+++ src/conexao/Conexao.java
@@ -1,10 +1,10 @@
 
 package conexao;
 
-import javax.swing.JOptionPane;
 import java.sql.*;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Conexao {
     final private String driver ="com.mysql.jdbc.Driver";
@@ -25,17 +25,24 @@ public class Conexao {
             alert.setTitle("Mensagem do Programa");
             alert.setHeaderText("Driver não localizado!");
             alert.setContentText(Driver.toString());
+            alert.getDialogPane().getStylesheets().add(getClass().getResource("/style/alert.css").toExternalForm());
+            alert.getDialogPane().getStyleClass().add("custom-alert");
+            ImageView icon = new ImageView(new Image(String.valueOf(this.getClass().getResource("/icons/Warning.png"))));
+            icon.setFitHeight(48);
+            icon.setFitWidth(48);
             alert.showAndWait();
-            System.exit(0);
-            
             result = false;
         } catch (SQLException Fonte) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Mensagem do Programa");
             alert.setHeaderText("Fonte de dados não encontrada!");
             alert.setContentText(Fonte.toString());
+            alert.getDialogPane().getStylesheets().add(getClass().getResource("/style/alert.css").toExternalForm());
+            alert.getDialogPane().getStyleClass().add("custom-alert");
+            ImageView icon = new ImageView(new Image(String.valueOf(this.getClass().getResource("/icons/Warning.png"))));
+            icon.setFitHeight(48);
+            icon.setFitWidth(48);
             alert.showAndWait();
-            System.exit(0);
             result = false;
         }
         return connection;
@@ -43,11 +50,15 @@ public class Conexao {
     
     public void disconnect() {
         try {
-            connection.close();
-            
+            connection.close();  
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Mensagem do Programa");
             alert.setHeaderText("Conexão com o banco fechada.");
+            alert.getDialogPane().getStylesheets().add(getClass().getResource("/style/alert.css").toExternalForm());
+            alert.getDialogPane().getStyleClass().add("custom-alert");
+            ImageView icon = new ImageView(new Image(String.valueOf(this.getClass().getResource("/icons/Warning.png"))));
+            icon.setFitHeight(48);
+            icon.setFitWidth(48);
             alert.showAndWait();
         } catch (SQLException fecha) {
             
@@ -63,6 +74,11 @@ public class Conexao {
             alert.setTitle("Mensagem do Programa");
             alert.setHeaderText("Erro no comando SQL!");
             alert.setContentText(excecao.toString());
+            alert.getDialogPane().getStylesheets().add(getClass().getResource("/style/alert.css").toExternalForm());
+            alert.getDialogPane().getStyleClass().add("custom-alert");
+            ImageView icon = new ImageView(new Image(String.valueOf(this.getClass().getResource("/icons/Warning.png"))));
+            icon.setFitHeight(48);
+            icon.setFitWidth(48);
             alert.showAndWait();
         }
     }
